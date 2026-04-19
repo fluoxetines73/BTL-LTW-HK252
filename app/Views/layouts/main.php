f8ebda0360b8b9b734347f934ad0de8749262cc7
danhnth <tdanh2005@gmail.com>
2026-04-20 00:01:27 +0700

    feat(layout): add Swiper.js CDN links to main layout

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -6,6 +6,7 @@
     <title><?= htmlspecialchars($title ?? 'Công Ty ABC') ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
     <?php if (!empty($extraHead)) echo $extraHead; ?>
     <link rel="stylesheet" href="<?= BASE_URL ?>public/css/style.css">
 </head>
@@ -19,6 +20,7 @@
 
     <?php include APPROOT . '/Views/layouts/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="<?= BASE_URL ?>public/js/app.js"></script>
     <?php if (!empty($extraScripts)) echo $extraScripts; ?>
 
